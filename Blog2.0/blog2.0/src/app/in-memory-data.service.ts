781bb123152d4dbca38c3d0dd1821703f97e5217
wroclawianka <dorota.zelga@gmail.com>
2017-12-29 15:22:59 +0100

    Ie memory data - more appropriate testing examples created

--- Blog2.0/blog2.0/src/app/in-memory-data.service.ts
+++ Blog2.0/blog2.0/src/app/in-memory-data.service.ts
@@ -6,19 +6,23 @@ export class InMemoryDataService implements InMemoryDbService {
     { 
       id: 1, 
       Title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", 
-      Text: `Quisque commodo tempus tortor, ut aliquet tellus dictum at. Nunc risus lectus, aliquet 
-        a pulvinar sit amet, pellentesque eget ipsum. Nulla pretium placerat justo, id condimentum nulla
-       congue egestas. Pellentesque dictum dapibus felis id efficitur. Mauris bibendum ac dui vel 
-       egestas. Vivamus in turpis odio. Praesent vel sapien eget leo hendrerit ultrices quis quis velit.`
+      Text: `Quisque commodo tempus tortor, ut aliquet tellus dictum at. Nunc risus lectus, aliquet a pulvinar sit amet, pellentesque eget ipsum. Nulla pretium placerat justo, id condimentum nulla congue egestas. Pellentesque dictum dapibus felis id efficitur. Mauris bibendum ac dui vel egestas. Vivamus in turpis odio. Praesent vel sapien eget leo hendrerit ultrices quis quis velit.`
     },
     { 
       id: 2, 
       Title: "Ut quis felis vestibulum, mollis tellus vitae, ultrices massa.", 
-      Text: `Vestibulum vitae porta nulla. Mauris lacinia dolor id libero scelerisque auctor. Praesent ut
-        leo ut velit interdum dapibus. Nam a pellentesque sapien. Fusce sit amet erat nec mauris aliquet tempus
-        ut et tellus. Sed vel nunc sapien. Donec ornare, diam at gravida consequat, mi quam posuere metus, ac 
-        auctor libero tortor non nulla. In ornare sit amet purus rutrum accumsan.`
+      Text: `Vestibulum vitae porta nulla. Mauris lacinia dolor id libero scelerisque auctor. Praesent ut leo ut velit interdum dapibus. Nam a pellentesque sapien. Fusce sit amet erat nec mauris aliquet tempus ut et tellus. Sed vel nunc sapien. Donec ornare, diam at gravida consequat, mi quam posuere metus, ac auctor libero tortor non nulla. In ornare sit amet purus rutrum accumsan.`
   },
+  { 
+    id: 3, 
+    Title: "Consectetur adipiscing elit.", 
+    Text: `Nunc risus lectus, aliquet a pulvinar sit amet, pellentesque eget ipsum. Nulla pretium placerat justo, id condimentum nulla congue egestas. Pellentesque dictum dapibus felis id efficitur. Mauris bibendum ac dui vel egestas. Vivamus in turpis odio. Praesent vel sapien eget leo hendrerit ultrices quis quis velit.`
+  },
+  { 
+    id: 4, 
+    Title: "MUt quis felis vestibulum, mollis tellus vitae, ultrices massa.", 
+    Text: `Mauris lacinia dolor id libero scelerisque auctor. Praesent ut leo ut velit interdum dapibus. Nam a pellentesque sapien. Fusce sit amet erat nec mauris aliquet tempus ut et tellus. Sed vel nunc sapien. Donec ornare, diam at gravida consequat, mi quam posuere metus, ac auctor libero tortor non nulla. In ornare sit amet purus rutrum accumsan.`
+},
     ];
     return {posts};
   }
