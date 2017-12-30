ff73430e020f675fa08b99b13e9166aef23448d6
wroclawianka <dorota.zelga@gmail.com>
2017-12-30 15:28:23 +0100

    Posts examples modified for testing purpose

--- BlogApi/Controllers/BlogPostController.cs
+++ BlogApi/Controllers/BlogPostController.cs
@@ -16,14 +16,26 @@ namespace BlogApi.Controllers
                 new BlogPostVM()
                 {
                     Id = 1,
-                    Text = "text1",
-                    Title = "title1"
+                    Title = "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                    Text = "Quisque commodo tempus tortor, ut aliquet tellus dictum at. Nunc risus lectus, aliquet a pulvinar sit amet, pellentesque eget ipsum. Nulla pretium placerat justo, id condimentum nulla congue egestas. Pellentesque dictum dapibus felis id efficitur. Mauris bibendum ac dui vel egestas. Vivamus in turpis odio. Praesent vel sapien eget leo hendrerit ultrices quis quis velit.",
                 },
                 new BlogPostVM()
                 {
-                    Text = "text2",
-                    Title = "title2",
-                    Id = 2
+                    Id = 2,
+                    Title = "Ut quis felis vestibulum, mollis tellus vitae, ultrices massa",
+                    Text = "Vestibulum vitae porta nulla. Mauris lacinia dolor id libero scelerisque auctor. Praesent ut leo ut velit interdum dapibus. Nam a pellentesque sapien. Fusce sit amet erat nec mauris aliquet tempus ut et tellus. Sed vel nunc sapien. Donec ornare, diam at gravida consequat, mi quam posuere metus, ac auctor libero tortor non nulla. In ornare sit amet purus rutrum accumsan.",
+                },
+                new BlogPostVM()
+                {
+                    Id = 3,
+                    Title = "Consectetur adipiscing elit",
+                    Text = "Nunc risus lectus, aliquet a pulvinar sit amet, pellentesque eget ipsum. Nulla pretium placerat justo, id condimentum nulla congue egestas. Pellentesque dictum dapibus felis id efficitur. Mauris bibendum ac dui vel egestas. Vivamus in turpis odio. Praesent vel sapien eget leo hendrerit ultrices quis quis velit.",
+                },
+                new BlogPostVM()
+                {
+                    Id = 4,
+                    Title = "Mut quis felis vestibulum, mollis tellus vitae, ultrices massa",
+                    Text = "Mauris lacinia dolor id libero scelerisque auctor. Praesent ut leo ut velit interdum dapibus. Nam a pellentesque sapien. Fusce sit amet erat nec mauris aliquet tempus ut et tellus. Sed vel nunc sapien. Donec ornare, diam at gravida consequat, mi quam posuere metus, ac auctor libero tortor non nulla. In ornare sit amet purus rutrum accumsan.",
                 }
             };
         }
