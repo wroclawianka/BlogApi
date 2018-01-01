bbaafed07c48f24b7f78f6e2bdc4da6c3dec5653
wroclawianka <dorota.zelga@gmail.com>
2018-01-01 16:29:35 +0100

    About Blog Page created

---
+++ Blog2.0/blog2.0/src/app/about-page/about-page.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-page',
+  templateUrl: './about-page.component.html',
+  styleUrls: ['./about-page.component.css', '../app.component.css', '../buttons.css']
+})
+export class AboutPageComponent implements OnInit {
+  header = "About Prague with my eyes blog";
+  content = `Welcome to my page! This is blog template for my learning purposes.
+    Here is a placeholder for a long text describing why this blog is awesome.
+    Right now I am focused on Angular with TypeScript, so this is the main 
+    technologies used by me. In the days when I am creating this template, 
+    I am at the very beginning of my adventure with front-end. Enjoy!`;
+  author = "- Dorota";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
