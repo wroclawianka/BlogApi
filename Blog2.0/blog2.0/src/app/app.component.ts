8b0ec16a9d16df2cb9b5f72f458043322de4eafe
wroclawianka <dorota.zelga@gmail.com>
2018-01-01 15:28:15 +0100

    Sidebar - sidebar goes to next row if width of the window is small

--- Blog2.0/blog2.0/src/app/app.component.ts
+++ Blog2.0/blog2.0/src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
- selector: 'app-root',
- templateUrl: 'app.component.html',
- styleUrls: ['app.component.css']
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css']
 })
 export class AppComponent {
- title = 'Prague';
- subtitle = 'with my eyes';
- footer = 'All Rights Reserved, Design and code by Dorota Zelga';
+    title = 'Prague';
+    subtitle = 'with my eyes';
+    footer = 'All Rights Reserved, Design and code by Dorota Zelga';
+
+    @HostListener('window:resize', ['$event'])
+    onResize(event) {
+        this.getGridTemplate();
+    }
+
+    getGridTemplate() {
+        const widthOfChange = 800;
+        return window.innerWidth <= widthOfChange ? this.setSideBarBelow() : this.setSideBarAbove();
+    }
+
+    setSideBarBelow() {
+        return `"header header" "nav nav" "content content" "sidebar sidebar" "footer footer"`;
+    }
+
+    setSideBarAbove() {
+        return `"header header" "nav nav" "content sidebar" "footer footer"`;
+    }
 }
