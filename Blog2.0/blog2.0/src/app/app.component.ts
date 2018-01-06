9347b06d992771abdbd2eb975205d6fc56aecb0d
wroclawianka <dorota.zelga@gmail.com>
2018-01-06 12:06:55 +0100

    refactoring - unused code removed

--- Blog2.0/blog2.0/src/app/app.component.ts
+++ Blog2.0/blog2.0/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, HostListener } from '@angular/core';
-// import { ContentLayout } from './contentLayout.module';
 
 @Component({
     selector: 'app-root',
@@ -10,28 +9,4 @@ export class AppComponent {
     title = 'Prague';
     subtitle = 'with my eyes';
     footer = 'All Rights Reserved, Design and code by Dorota Zelga';
-    // contentLayout : ContentLayout = new ContentLayout(); //!!!!
-
-    // @HostListener('window:resize', ['$event'])
-    // onResize(event) {
-    // }
-
-    // getGridTemplate() {
-    //     const widthOfChange = 800;
-    //     return window.innerWidth <= widthOfChange ? this.setSideBarBelow() : this.setSideBarAbove();
-    // }
-
-    // setSideBarBelow() {
-    //     return `"posts-list" "sidebar"`;
-    // }
-
-    // setSideBarAbove() {
-    //     return `"posts-list sidebar"`;
-    // }
-
-    // ngOnInit() {
-    //     this.getGridTemplate();
-    // }
-
-// }
 }
--- Blog2.0/blog2.0/src/app/app.module.ts
+++ Blog2.0/blog2.0/src/app/app.module.ts
@@ -3,9 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
-import { HttpClientModule } from '@angular/common/http';
-//mock 
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api'; //mock
 import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
