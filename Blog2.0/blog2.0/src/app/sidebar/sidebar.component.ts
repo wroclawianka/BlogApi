4e307d6239b39f8e053c141f048f228f5e86af9c
wroclawianka <dorota.zelga@gmail.com>
2018-01-14 12:52:11 +0100

    Sidebar - external links added

--- Blog2.0/blog2.0/src/app/sidebar/sidebar.component.ts
+++ Blog2.0/blog2.0/src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Reference } from '../reference';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,12 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+  
   aboutMeHeader = "About me";
   aboutMeContent = `Welcome to my blog template project. 
   This page is created for my self-learning purposes. 
   <br> I am training frontend skills, so I will practice here 
-  Angular & TypeScript with other tools and frameworks. 
-  <br>To see more of my project, please, visit my GitHub profile.`;
+  Angular & TypeScript with other tools and frameworks.`;
+
+  references: Reference[] = [
+    {
+      url:"https://github.com/wroclawianka",
+      icon:"fa fa-github", 
+      text:" /wroclawianka"
+    },
+    {
+      url:"https://www.linkedin.com/in/dorotazelga",
+      icon:"fa fa-linkedin", 
+      text:" /dorotazelga"
+    }
+  ]
 
   constructor() { }
 
