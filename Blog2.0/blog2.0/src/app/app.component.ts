bce7c79c3e9c7ffa2993d13d8d7bf51e79d78ff7
wroclawianka <dorota.zelga@gmail.com>
2018-02-28 20:06:38 +0100

    Playground - switch created

--- Blog2.0/blog2.0/src/app/app.component.ts
+++ Blog2.0/blog2.0/src/app/app.component.ts
@@ -4,7 +4,7 @@ import * as $ from 'jquery';
 @Component({
     selector: 'app-root',
     templateUrl: 'app.component.html',
-    styleUrls: ['app.component.css', '../styles/buttons.css']
+    styleUrls: ['app.component.css', '../styles/buttons.css', '../styles/switch.css']
 })
 export class AppComponent {
     title = 'Prague';
